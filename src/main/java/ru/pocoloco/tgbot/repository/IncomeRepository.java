5aba9c7de249df68349a2092f1663dd85dfbf97a
Schraibikus <kurt-77@mail.ru>
2023-07-08 12:41:31 +0500

    пункт 23 (не работает БД, как фиксить хз)

---
+++ src/main/java/ru/pocoloco/tgbot/repository/IncomeRepository.java
@@ -0,0 +1,9 @@
+package ru.pocoloco.tgbot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.pocoloco.tgbot.entity.Income;
+
+@Repository
+public interface IncomeRepository extends JpaRepository<Income, Long> {
+}
